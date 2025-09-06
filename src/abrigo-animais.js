1f86a3ba4a085a6361e27e6d5a724edd344b15ab
Daniel Antunes <dev.daniel.antunes@gmail.com>
2025-09-06 04:04:44 -0300

    feat(abrigo-animais): add método para validar animal através da ordem fornecida, recusando duplicados ou inexistentes

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,5 +1,29 @@
+import { Pessoa } from "./pessoa.js";
+import { animais} from "./dados-animais.js"
+
 class AbrigoAnimais {
-  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+	
+	encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+	if (!this.validarAnimal(ordemAnimais))  {
+		return {erro: 'Animal inválido'}
+	}
+}
+
+  validarAnimal(ordemAnimais) {
+	let listaAnimais = ordemAnimais.split(',')
+	listaAnimais.sort()
+	let animaisUnicos = []
+	for (let animal of listaAnimais) {
+		animal = animal.trim()
+		if (animaisUnicos.includes(animal)) {
+			return false
+		}
+		if (!animais.includes(animal)) {
+			return false
+		}
+		animaisUnicos.push(animal)
+	}
+	return true
   }
 }
 
